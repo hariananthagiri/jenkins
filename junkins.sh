cd7684abc074a5ba4de18e68ea0b82467f55b82d
hariananthagiri <hariananthagiri@gmail.com>
2024-08-21 19:02:49 +0530

    initial commit

--- junkins.sh
+++ junkins.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo su -
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key  
 sudo yum install fontconfig java-17-openjdk -y
