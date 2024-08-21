bcd619095c750e673db0b26479f9510094a6e6ec
hariananthagiri <hariananthagiri@gmail.com>
2024-08-21 16:42:19 +0530

    initial commit

---
+++ junkins.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key  
+sudo yum install fontconfig java-17-openjdk -y
+sudo yum install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo systemctl enable jenkins
